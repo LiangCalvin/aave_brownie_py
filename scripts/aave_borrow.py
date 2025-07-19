817ff35e5d7774adc2ab05eb2324705a5473bec9
Calvin Liang <145249380+LiangCalvin@users.noreply.github.com>
2025-07-19 14:40:18 +0700

    add get_asset_price, get_borrowable_data

--- scripts/aave_borrow.py
+++ scripts/aave_borrow.py
@@ -4,6 +4,8 @@ from scripts.get_weth import get_weth
 from scripts.helpful_scripts import get_account
 
 amount = Web3.to_wei(0.1, "ether")
+
+
 def main():
     """
     Main function to execute the Aave borrowing script.
@@ -21,20 +23,53 @@ def main():
     # Approve sending out ERC20 tokens
     approve_erc20(amount, lending_pool.address, erc20_address, account)
     print("Depositing...")
-    tx = lending_pool.deposit(erc20_address, amount, account.address, 0, {"from": account})
+    tx = lending_pool.deposit(
+        erc20_address, amount, account.address, 0, {"from": account}
+    )
     tx.wait(1)
     print("Deposited!")
+    # how much
+
+
+def get_asset_price(price_feed_address):
+    # ABI
+    # Address
+    dai_eth_price_feed = interface.AggregatorV3Interface(price_feed_address)
+    latest_price = dai_eth_price_feed.latestRoundData()[1]
+    converted_latest_price = Web3.from_wei(latest_price, "ether")
+    print(f"The DAI/ETH price is {converted_latest_price}")
+    # 0.000279254836104738
+    return float(converted_latest_price)
+
+
+def get_borrowable_data(lending_pool, account):
+    (
+        total_collateral_eth,
+        total_debt_eth,
+        available_borrow_eth,
+        current_liquidation_threshold,
+        ltv,
+        health_factor,
+    ) = lending_pool.getUserAccountData(account.address)
+    available_borrow_eth = Web3.from_wei(available_borrow_eth, "ether")
+    total_collateral_eth = Web3.from_wei(total_collateral_eth, "ether")
+    total_debt_eth = Web3.from_wei(total_debt_eth, "ether")
+    print(f"You have total collateral {total_collateral_eth} worth of ETH deposited.")
+    print(f"You have total debt {total_debt_eth} worth of ETH borrowed.")
+    print(f"You have available borrow {available_borrow_eth} worth of ETH.")
+    return (float(available_borrow_eth), float(total_debt_eth))
 
 
 def approve_erc20(amount, spender, erc20_address, account):
+    # ABI
+    # Address
     print("Approving ERC20 token...")
     erc20 = interface.IERC20(erc20_address)
     tx = erc20.approve(spender, amount, {"from": account})
     tx.wait(1)
     print("Approved!")
     return tx
-    # ABI
-    # Address
+
 
 def get_lending_pool():
     # ABI
