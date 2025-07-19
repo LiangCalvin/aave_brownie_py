3e3356d461ae288eb3840c5f873dd818037f12b5
Calvin Liang <145249380+LiangCalvin@users.noreply.github.com>
2025-07-19 15:48:40 +0700

    refactor get_weth

--- scripts/get_weth.py
+++ scripts/get_weth.py
@@ -2,13 +2,17 @@ from scripts.helpful_scripts import get_account
 from brownie import network, config, Contract, interface
 from web3 import Web3
 
-def get_weth():
-    account = get_account()
-    weth = interface.IWeth(config["networks"][network.show_active()]["weth_token"])
-    tx = weth.deposit({"from": account, "value": 0.1 * 10**18})  # Deposit 0.1 ETH to get WETH
-    tx.wait(1)
-    print("Received 0.1 WETH")
-    return tx
 
 def main():
     get_weth()
+
+
+def get_weth():
+    account = get_account()
+    weth = interface.IWeth(config["networks"][network.show_active()]["weth_token"])
+    tx = weth.deposit(
+        {"from": account, "value": 0.1 * 10**18}
+    )  # Deposit 0.1 ETH to get WETH
+    tx.wait(1)
+    print("Received 0.1 WETH")
+    return tx
